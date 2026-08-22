5a1406aad3ef6330875a5331fd5bfd5ca5e00285
Whiley True <whiletrue500@gmail.com>
2026-08-22 06:52:26 +0100

    resetting repo without click

--- src/inreach/app/setup/__init__.py
+++
@@ -1,5 +0,0 @@
-from inreach.app.setup.menu import run_init_menu
-from inreach.app.setup.project import create_project
-from inreach.app.setup.setup_proj import setup_project
-
-__all__ = ["create_project", "run_init_menu", "setup_project"]
